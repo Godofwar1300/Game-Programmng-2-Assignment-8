0b8436b53a272085438812d729e4834991cbde4a
Godofwar1300 <55067215+Godofwar1300@users.noreply.github.com>
2020-03-09 14:37:13 -0500

    Started the assignment and got basic menus in

---
+++ Game Programming 2 Assignment 7 Template Method Pattern/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public static ButtonManager instance;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(instance);
+        }
+
+        DontDestroyOnLoad(instance);
+    }
+
+
+    public void LoadLevel(string levelName)
+    {
+        switch (levelName)
+        {
+            case "Main Menu":
+                SceneManager.LoadScene(0);
+                break;
+
+            case "Game":
+                SceneManager.LoadScene(1);
+                break;
+
+            case "Instructions":
+                SceneManager.LoadScene(2);
+                break;
+        }
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+}
